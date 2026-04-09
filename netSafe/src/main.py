4f1b930f039e11827d9e99a7b6cda7fb59944914
Arthur Gabriel Moreira <arthurgmoreirac@gmail.com>
2026-04-09 11:47:19 -0300

    Organizando estrutura do projeto

--- netSafe/src/main.py
+++ netSafe/src/main.py
@@ -1,24 +1,29 @@
-import sys
-from scanner import scan_ports
+from scanner import scan_ports, scan_network
 from utils import classify_port
+import sys
 
 def main():
     if len(sys.argv) < 2:
-        print("Uso: python main.py <IP>")
+        print("Uso: python main.py <base_ip>")
         return
 
-    ip = sys.argv[1]
-    ports = scan_ports(ip)
+    base_ip = sys.argv[1]
 
-    print(f"[+] {ip} - RESULTADO")
+    print("[*] Escaneando rede...")
 
-    if not ports:
-        print("Nenhuma porta aberta encontrada.")
-        return
+    hosts = scan_network(base_ip)
 
-    for port in ports:
-        risk = classify_port(port)
-        print(f"Porta {port} {risk}")
+    for ip in hosts:
+        print(f"\n[+] {ip} - ATIVO")
+        ports = scan_ports(ip)
+
+        if not ports:
+            print("Sem portas abertas")
+            continue
+
+        for port in ports:
+            risk = classify_port(port)
+            print(f"Porta {port} {risk}")
 
 if __name__ == "__main__":
     main()
--- netSafe/src/scanner.py
+++ netSafe/src/scanner.py
@@ -1,6 +1,8 @@
 import socket
+import subprocess
 
 COMMON_PORTS = [21, 22, 23, 80, 443, 3389]
+
 
 def scan_ports(ip):
     open_ports = []
@@ -16,3 +18,25 @@ def scan_ports(ip):
         sock.close()
 
     return open_ports
+
+
+def ping_host(ip):
+    try:
+        output = subprocess.run(
+            ["ping", "-n", "1", "-w", "500", ip],
+            stdout=subprocess.DEVNULL
+        )
+        return output.returncode == 0
+    except:
+        return False
+
+
+def scan_network(base_ip):
+    active_hosts = []
+
+    for i in range(1, 255):
+        ip = f"{base_ip}.{i}"
+        if ping_host(ip):
+            active_hosts.append(ip)
+
+    return active_hosts
