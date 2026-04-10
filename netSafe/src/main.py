e09c62afef31ab52c925d8fbdd205a8e401128ee
Arthur Gabriel Moreira <arthurgmoreirac@gmail.com>
2026-04-10 07:57:49 -0300

    Alteração main.py

--- netSafe/src/main.py
+++ netSafe/src/main.py
@@ -2,16 +2,29 @@ from scanner import scan_ports, scan_network
 from utils import classify_port
 import sys
 
+__version__ = "1.0.0"
+
 def main():
+    print(f"--- NetSafe Scanner v{__version__} ---")
     if len(sys.argv) < 2:
-        print("Uso: python main.py <base_ip>")
+        print("Uso: python main.py <ip_alvo>")
         return
 
-    base_ip = sys.argv[1]
+    alvo_ip = sys.argv[1]
+    
+    partes = alvo_ip.split('.')
+    if len(partes) == 4:
+        base_ip = f"{partes[0]}.{partes[1]}.{partes[2]}"
+    else:
+        base_ip = alvo_ip
 
-    print("[*] Escaneando rede...")
+    print(f"[*] Escaneando rede base: {base_ip}.X ... (Pode demorar 1 ou 2 minutos)")
 
     hosts = scan_network(base_ip)
+
+    if not hosts:
+        print("[-] Nenhum dispositivo ativo encontrado. Tente outro IP.")
+        return
 
     for ip in hosts:
         print(f"\n[+] {ip} - ATIVO")
