be526e5d188cc9fb341941f0dc2f550f73088411
Arthur Gabriel Moreira <arthurgmoreirac@gmail.com>
2026-04-11 21:20:06 -0300

    feat: adiciona validacao de seguranca no input de IP

--- netSafe/src/main.py
+++ netSafe/src/main.py
@@ -5,13 +5,27 @@ import sys
 def main():
     if len(sys.argv) < 2:
         print("Uso: python main.py <base_ip>")
+        print("Exemplo: python main.py 192.168.1")
         return
 
     base_ip = sys.argv[1]
-
-    print("[*] Escaneando rede...")
+    
+    partes_ip = base_ip.split('.')
+    if len(partes_ip) != 3:
+        print("\n[!] ERRO: Formato de IP inválido!")
+        print("[!] Deves inserir apenas os 3 primeiros blocos (octetos) da rede.")
+        print("[!] Correto: 192.168.1")
+        print("[!] Incorreto: 192.168.1.15\n")
+        return
+    
+    print(f"[*] A explorar a rede {base_ip}.X ...")
 
     hosts = scan_network(base_ip)
+
+   
+    if not hosts:
+        print("[-] Nenhum dispositivo ativo encontrado nesta rede.")
+        return
 
     for ip in hosts:
         print(f"\n[+] {ip} - ATIVO")
